21a048f4228ad73f8e640c0a6bb61c24e7079d66
Paul Frazee <pfrazee@gmail.com>
2015-09-19 15:20:36 -0500

    move ui flow into widgets, to keep feeds from getting too big

--- feeds.js
+++ feeds.js
@@ -9,7 +9,6 @@ var screen = blessed.screen({
   dockBorders: true
 })
 var windows = require('./lib/windows')(screen)
-var feedsList = require('./lib/widgets/feedslist')(screen)
 
 screen.key(['C-c'], function(ch, key) {
   return process.exit(0)
@@ -17,26 +16,5 @@ screen.key(['C-c'], function(ch, key) {
 
 require('./sbot')(function (err, sbot) {
   if(err) throw err
-
-  // load and run toplevel view, the feeds list
-  feedsList.data.load(sbot, function () {
-    windows.push(feedsList)
-  })
-
-  // listen for feed selections
-  feedsList.on('select', function (el, selected) {
-    // load up a log list with the selected feed
-    var logList = require('./lib/widgets/loglist')(screen)
-    logList.data.load(sbot, feedsList.data.feeds[selected].id, function () {
-      windows.push(logList)
-    })
-
-    // listen for log-entry selections
-    logList.on('select', function (el, selected) {
-      // load up a message view with the selected message
-      var msgView = require('./lib/widgets/msgview')(screen)
-      msgView.content = JSON.stringify(logList.data.log[selected], 0, 2)
-      windows.replace(logList, msgView, { noFocus: true })
-    })
-  })
+  windows.push(require('./lib/widgets/feedslist')(screen, windows, sbot))
 })
