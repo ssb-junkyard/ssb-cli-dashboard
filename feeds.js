3c6d7a60aff672adf3703b2dc7a2f18f2a945d96
Paul Frazee <pfrazee@gmail.com>
2015-09-19 11:52:40 -0500

    move more behaviors into widgets

--- feeds.js
+++ feeds.js
@@ -9,7 +9,7 @@ var screen = blessed.screen({
   dockBorders: true
 })
 var windows = require('./lib/windows')(screen)
-var feedList = require('./lib/widgets/feedlist')(screen)
+var feedsList = require('./lib/widgets/feedslist')(screen)
 var logList = require('./lib/widgets/loglist')(screen)
 var msgView = require('./lib/widgets/msgview')(screen)
 
@@ -20,54 +20,19 @@ screen.key(['C-c'], function(ch, key) {
 require('./sbot')(function (err, sbot) {
   if(err) throw err
 
-  var done = multicb({ pluck: 1 })
-  pull(sbot.latest(), pull.collect(done()))
-  sbot.friends.all('follow', done())
-  sbot.friends.all('flag', done())
-  done(function (err, res) {
-    if (err) throw err
-    var feeds = res[0]
-    feedList.setItems(feedsToListData.apply(null, res))
-    windows.push(feedList)
+  feedsList.data.load(sbot, function () {
+    windows.push(feedsList)
+  })
 
-    feedList.on('select', function (el, selected) {
-      logList.setLabel(' Feed: ' + feeds[selected].id + ' ')
+  feedsList.on('select', function (el, selected) {
+    logList.data.load(sbot, feedsList.data.feeds[selected].id, function () {
+      windows.push(logList)
 
-      pull(sbot.createUserStream({ id: feeds[selected].id }), pull.collect(function (err, entries) {
-        if (err) throw err
-        entries.reverse()
-        logList.setItems(entries.map(function (msg) { return JSON.stringify(msg.value.content) }))
-        logList.select(0)
-        windows.push(logList)
-
-        logList.removeAllListeners('select')
-        logList.on('select', function (el, selected) {
-          msgView.content = JSON.stringify(entries[selected], 0, 2)
-          windows.push(msgView, { noFocus: true })
-        })
-      }))
+      logList.removeAllListeners('select')
+      logList.on('select', function (el, selected) {
+        msgView.content = JSON.stringify(logList.data.log[selected], 0, 2)
+        windows.push(msgView, { noFocus: true })
+      })
     })
   })
 })
-
-function countInbounds (graph, id) {
-  var n = 0
-  for (var id2 in graph)
-    if (graph[id2][id])
-      n++
-  return n
-}
-
-function feedsToListData (feeds, follows, flags) {
-  feeds.sort(function (a, b) {
-    return b.sequence - a.sequence
-  })
-
-  return feeds.map(function (f) {
-    return f.id 
-      + ' [seq: ' + f.sequence 
-      + ' follows: ' + Object.keys(follows[f.id] || {}).length + '/' + countInbounds(follows, f.id)
-      + ' flags: ' + Object.keys(flags[f.id] || {}).length + '/' + countInbounds(flags, f.id)
-      + ']'
-  })
-}
