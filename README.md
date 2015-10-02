33c13e1892a19ec54c6a9d2e2f5d5cb5e286fa3f
Paul Frazee <pfrazee@gmail.com>
2015-10-02 14:19:02 -0500

    cleanup and document feeds.js

--- README.md
+++ README.md
@@ -26,3 +26,71 @@ $ ./gossip.js
 ```
 
 ![./demo.gif](./demo.gif)
+
+## How it works
+
+The `feeds.js` view pulls some metadata from Scuttlebot, and uses that to produce a master list of known feeds.
+
+```js
+// load data from sbot
+var done = multicb({ pluck: 1, spread: true })
+
+pull(
+  sbot.latest(),                          // get the sequence number of the latest message of each known feed
+  (filter) ? pull.filter(filter) : null,  // apply a filter, if given
+  pull.collect(done())                    // collect into an array
+)
+sbot.friends.all('follow', done())        // fetch the computed follow-graph
+sbot.friends.all('flag', done())          // fetch the computed flag-graph
+
+done(function (err, feeds, follows, flags) {
+  // ...
+})
+```
+
+Each entry shows:
+
+```
+feedid [seq: N follows: N/N flags: N/N]
+```
+
+Where, in the case of follows and flags, it's showing the outbound then inbound.
+It computes this info by counting directed-edges:
+
+```js
+// helper to count how many nodes in the graph have edges pointing to the given ID
+// - graphs are given in the shape of { sourceIds: { destIds: true } }
+// - eg. if bob follows alice, the follow graph would include { bobsId: { alicesId: true } }
+// - if alice also followed bob, the follow graph would be { bobsId: { alicesId: true }, alicesId: { bobsId: true } }
+function countInbounds (graph, id) {
+  var n = 0
+  for (var id2 in graph)
+    if (graph[id2][id])
+      n++
+  return n
+}
+
+// helper to count how many nodes in the graph the given ID points to
+// - see `countInbounds` comment for more info
+function countOutbounds(graph, id) {
+  return Object.keys(graph[id] || {}).length
+}
+
+// take the feeds and graphs, produce textual list items
+function feedsToListItems (feeds, follows, flags) {
+  // sort by highest sequence to lowest sequence
+  feeds.sort(function (a, b) {
+    return b.sequence - a.sequence
+  })
+
+  // produce a list of labels
+  return feeds.map(function (f) {
+    var info = [
+      'seq: '     + f.sequence,
+      'follows: ' + countOutbounds(follows, f.id) + '/' + countInbounds(follows, f.id),
+      'flags: '   + countOutbounds(flags, f.id)   + '/' + countInbounds(flags, f.id)
+    ]
+    return f.id + '[' + info.join(' ') + ']'
+  })
+}
+```
--- feeds.js
+++ feeds.js
@@ -8,26 +8,41 @@ module.exports = function (sbot, screen, filter) {
 
   // load data from sbot
   var done = multicb({ pluck: 1, spread: true })
-  pull(sbot.latest(), (filter) ? pull.filter(filter) : null, pull.collect(done()))
-  sbot.friends.all('follow', done())
-  sbot.friends.all('flag', done())
+
+  pull(
+    sbot.latest(),                          // get the sequence number of the latest message of each known feed
+    (filter) ? pull.filter(filter) : null,  // apply a filter, if given
+    pull.collect(done())                    // collect into an array
+  )
+  sbot.friends.all('follow', done())        // fetch the computed follow-graph
+  sbot.friends.all('flag', done())          // fetch the computed flag-graph
+
   done(function (err, feeds, follows, flags) {
     if (err) throw err
+    // populate the list widget and render
+    var listItems = feedsToListItems(feeds, follows, flags)
     listWidget.data.feeds = feeds
-    listWidget.setItems(feedsToListData(feeds, follows, flags))
+    listWidget.setItems(listItems)
     screen.render()
   })
 
   // listen for feed selections
   listWidget.on('select', function (el, selected) {
-    if (!listWidget.data.feeds[selected])
+    var selectedFeed = listWidget.data.feeds[selected]
+    if (!selectedFeed)
       return
-    screen.stack.push(require('./feed')(listWidget.data.feeds[selected].id, sbot, screen))
+    // open the feed view for the selected feed
+    var feedView = require('./feed')(selectedFeed.id, sbot, screen)
+    screen.stack.push(feedView)
   })
 
   return listWidget
 }
 
+// helper to count how many nodes in the graph have edges pointing to the given ID
+// - graphs are given in the shape of { sourceIds: { destIds: true } }
+// - eg. if bob follows alice, the follow graph would include { bobsId: { alicesId: true } }
+// - if alice also followed bob, the follow graph would be { bobsId: { alicesId: true }, alicesId: { bobsId: true } }
 function countInbounds (graph, id) {
   var n = 0
   for (var id2 in graph)
@@ -36,17 +51,27 @@ function countInbounds (graph, id) {
   return n
 }
 
-function feedsToListData (feeds, follows, flags) {
+// helper to count how many nodes in the graph the given ID points to
+// - see `countInbounds` comment for more info
+function countOutbounds(graph, id) {
+  return Object.keys(graph[id] || {}).length
+}
+
+// take the feeds and graphs, produce textual list items
+function feedsToListItems (feeds, follows, flags) {
+  // sort by highest sequence to lowest sequence
   feeds.sort(function (a, b) {
     return b.sequence - a.sequence
   })
 
+  // produce a list of labels
   return feeds.map(function (f) {
-    return f.id 
-      + ' [seq: ' + f.sequence 
-      + ' follows: ' + Object.keys(follows[f.id] || {}).length + '/' + countInbounds(follows, f.id)
-      + ' flags: ' + Object.keys(flags[f.id] || {}).length + '/' + countInbounds(flags, f.id)
-      + ']'
+    var info = [
+      'seq: '     + f.sequence,
+      'follows: ' + countOutbounds(follows, f.id) + '/' + countInbounds(follows, f.id),
+      'flags: '   + countOutbounds(flags, f.id)   + '/' + countInbounds(flags, f.id)
+    ]
+    return f.id + ' [' + info.join(' ') + ']'
   })
 }
 
